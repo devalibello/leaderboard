035fed46314dd8183551b0d4966b881d77355630
devalibello <alibelloimoukhuede@gmail.com>
2023-05-25 05:38:20 +0300

    Add api calls

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 import './style.css';
-import score from './modules/scores.js';
+import PostObject from './modules/postobject.js';
 
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/microverse-ali/scores/';
+const getButton = document.getElementById('refresh-btn');
 const scoreContainer = document.getElementById('names-and-scores');
-for (let i = 0; i < score.scores.length; i += 1) {
-  scoreContainer.innerHTML += `
-  <li class='name-color-${i % 2} name-element'>Name: ${score.scores[i]}</li>
-  `;
-}
+const addGame = document.getElementById('submit-btn');
+const userName = document.getElementById('user-name');
+const userScore = document.getElementById('user-score');
+
+// EVENT LISTENER FOR ADD BTN AND POST API
+
+addGame.addEventListener('click', async () => {
+  const newPostObject = new PostObject(userName.value, userScore.value);
+  userName.value = '';
+  userScore.value = '';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPostObject),
+  };
+  await fetch(url, options)
+    .then((response) => response.json());
+});
+
+// FOR API GET
+
+getButton.addEventListener('click', async () => {
+  await fetch(url).then((response) => response.json())
+    .then((responseData) => {
+      const realData = responseData.result;
+      scoreContainer.innerHTML = '';
+      for (let i = 0; i < realData.length; i += 1) {
+        scoreContainer.innerHTML += `
+                <li class='name-color-${i % 2} name-element'>${realData[i].user}: ${realData[i].score}</li>
+                `;
+      }
+    });
+});
